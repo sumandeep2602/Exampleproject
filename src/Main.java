6474fe77f380d89afd6c8ec18fb7171c90c876cc
macstudent <macstudent@10.10.150.116>
2019-09-05 12:56:53 -0400

    code addeed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-
+		System.out.println("This is my project");
+		System.out.println("hlo project");
+		System.out.println("project");
 	}
 
 }
